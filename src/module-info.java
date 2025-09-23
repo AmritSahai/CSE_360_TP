7e63482ee9641e11acab45eddc6dc3fdc9f466ce
Joseph Kim <josephjunseo0720@gmail.com>
2025-09-22 20:09:32 -0700

    Update module-info: add javafx.graphics, javafx.fxml; open GUI packages

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,18 @@
 module FoundationsF25 {
 	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
 	requires java.sql;
 	
 	opens applicationMain to javafx.graphics, javafx.fxml;
+	opens guiFirstAdmin to javafx.graphics, javafx.fxml;
+	opens guiUserLogin to javafx.graphics, javafx.fxml;
+	opens guiAdminHome to javafx.graphics, javafx.fxml;
+	opens guiNewAccount to javafx.graphics, javafx.fxml;
+	opens guiUserUpdate to javafx.graphics, javafx.fxml;
+	opens guiAddRemoveRoles to javafx.graphics, javafx.fxml;
+	opens guiMultipleRoleDispatch to javafx.graphics, javafx.fxml;
+	opens guiRole1 to javafx.graphics, javafx.fxml;
+	opens guiRole2 to javafx.graphics, javafx.fxml;
+	opens guiTools to javafx.graphics, javafx.fxml;
 }
